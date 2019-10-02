b132863e0c29e3a3c46a97f22f5a9b69ee4ec83c
DarkEyeDragon <joshua.tack@student.vives.be>
2019-10-02 23:12:20 +0200

    Improved protection against malicious image links

---
+++ src/main/java/me/darkeyedragon/imageutils/client/image/ImageType.java
@@ -0,0 +1,26 @@
+package me.darkeyedragon.imageutils.client.image;
+
+public enum ImageType {
+
+    PNG(0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A),
+    JPG(0xFF, 0xD8, 0xFF, 0xDB),
+    TIFF(0x4D, 0x4D, 0x00, 0x2A);
+
+    final int[] hexValue;
+
+    ImageType(int... hexValue) {
+        this.hexValue = hexValue;
+    }
+
+    public int[] getHexValue() {
+        return hexValue;
+    }
+
+    public boolean compare(int... toCompare) {
+        if (toCompare.length != hexValue.length) return false;
+        for (int i = 0; i < hexValue.length; i++) {
+            if (toCompare[i] != hexValue[i]) return false;
+        }
+        return true;
+    }
+}
--- src/main/java/me/darkeyedragon/imageutils/client/util/ImageUtil.java
+++ src/main/java/me/darkeyedragon/imageutils/client/util/ImageUtil.java
@@ -1,8 +1,18 @@
 package me.darkeyedragon.imageutils.client.util;
 
 import me.darkeyedragon.imageutils.client.ImageUtilsMain;
+import me.darkeyedragon.imageutils.client.ScreenshotHandler;
+import me.darkeyedragon.imageutils.client.image.ImageType;
 import me.darkeyedragon.imageutils.client.message.ClientMessage;
 import net.coobird.thumbnailator.Thumbnails;
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.text.TextComponentString;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
@@ -25,26 +35,69 @@ public class ImageUtil {
         }
     }
 
-    public static BufferedImage downloadFromUrl(String url, Map<String, BufferedImage> linkList) throws IOException {
 
-        if (StringFilter.isValidUrl(url)) {
-            URL imgUrl = new URL(url);
-            if (!StringFilter.isValidImage(url)) {
-                imgUrl = new URL(url + ".png");
+    /**
+     * Checks if an url contains a valid image by checking the first 8 bytes of the content body. They represent an {@link ImageType}
+     *
+     * @param imageUrl the url of the image
+     * @return returns true if the first bytes represent a image type
+     */
+    public static boolean isValidImage(String imageUrl) {
+        HttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(imageUrl);
+        httpGet.addHeader(HttpHeaders.USER_AGENT, ImageUtilsMain.NAME + "/" + ImageUtilsMain.VERSION);
+        httpGet.addHeader(HttpHeaders.RANGE, "bytes=0-7");
+        try {
+            HttpResponse response = httpClient.execute(httpGet);
+            HttpEntity httpEntity = response.getEntity();
+            if (httpEntity.getContentLength() > 8) return false;
+            Minecraft.getMinecraft().ingameGUI.getChatGUI().printChatMessage(new TextComponentString("" + response.getEntity().getContentLength()));
+            int[] bytes = new int[8];
+            for (int i = 0; i < 8; i++) {
+                bytes[i] = (short) httpEntity.getContent().read();
+                Minecraft.getMinecraft().ingameGUI.getChatGUI().printChatMessage(new TextComponentString("" + bytes[i]));
             }
-            if (StringFilter.isValidImage(url)) {
-                URLConnection conn;
-                conn = imgUrl.openConnection();
-                conn.setRequestProperty("User-Agent", "ScreenshotUploader/" + ImageUtilsMain.VERSION);
-                InputStream image = conn.getInputStream();
-                BufferedImage img = ImageIO.read(image);
-                linkList.put(imgUrl.toString(), img);
+            for (int i = 0; i < ImageType.values().length; i++) {
+                if (ImageType.values()[i].compare(bytes)) return true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        /*try (CloseableHttpClient curClient = HttpClientBuilder.create().setUserAgent("Image Utils/" + ImageUtilsMain.VERSION).build()) {
+            return curClient.execute(new HttpGet(imageUrl)).getFirstHeader("Content-Type").getValue().startsWith("image/");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+        return false;
+    }
+
+    public static BufferedImage downloadFromUrl(String url) throws IOException {
+
+        if (!StringFilter.isValidUrl(url) || !isValidImage(url)) {
+            return null;
+        }
+        URL imgUrl = new URL(url);
+        if (isValidImage(url)) {
+            URLConnection conn;
+            conn = imgUrl.openConnection();
+            conn.setRequestProperty(HttpHeaders.USER_AGENT, ImageUtilsMain.NAME + "/" + ImageUtilsMain.VERSION);
+            InputStream image = conn.getInputStream();
+            BufferedImage img = ImageIO.read(image);
+            Map<String, BufferedImage> downloads = ScreenshotHandler.getDownloadList();
+            if (!downloads.containsKey(imgUrl.toString())) {
+                ScreenshotHandler.getDownloadList().put(imgUrl.toString(), img);
                 return img;
+
+            } else {
+                return downloads.get(imgUrl.toString());
             }
         }
         return null;
     }
 
+    public void getImageSize(String url) {
+
+    }
 
     public static BufferedImage getLocal(File location) throws IOException {
         return ImageIO.read(location);
